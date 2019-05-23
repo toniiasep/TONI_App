7b09f8704ac3a9665471bc316419bf35cdef352f
toniasep <toni@sawala.co>
2019-05-23 15:33:23 +0700

    profile, contact

--- app/src/main/java/com/toniasep/MainActivity.java
+++ app/src/main/java/com/toniasep/MainActivity.java
@@ -9,6 +9,7 @@
 package com.toniasep;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -167,4 +168,6 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+
 }
+
--- app/src/main/java/com/toniasep/view/HomeActivity.java
+++ app/src/main/java/com/toniasep/view/HomeActivity.java
@@ -8,12 +8,15 @@
 
 package com.toniasep.view;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.toniasep.R;
 import com.toniasep.presenter.ContactFragment;
@@ -55,4 +58,51 @@ public class HomeActivity extends AppCompatActivity {
                     return true;
                 }
             };
+
+    public void Call(View view){
+        Intent telepon = new Intent(Intent.ACTION_DIAL);
+        telepon.setData(Uri.parse("tel:08228329336"));
+        startActivity(telepon);
+
+    }
+    public void ig(View view){
+        Intent ig = new Intent(Intent.ACTION_VIEW);
+        ig.setData(Uri.parse("https://instagram.com/toniiasep"));
+        startActivity(ig);
+
+    }
+    public void fb(View view){
+        Intent fb = new Intent(Intent.ACTION_VIEW);
+        fb.setData(Uri.parse("https://facebook.com/toniiasep"));
+        startActivity(fb);
+
+    }
+    public void yt(View view){
+        Intent yt = new Intent(Intent.ACTION_VIEW);
+        yt.setData(Uri.parse("https://youtube.com/tonichannel"));
+        startActivity(yt);
+
+    }
+    public void in(View view){
+        Intent in = new Intent(Intent.ACTION_VIEW);
+        in.setData(Uri.parse("https://www.linkedin.com/in/toniasep/"));
+        startActivity(in);
+
+    }
+
+    public void email(View view){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"toniiasep@gmail.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Hai Developer");
+        intent.putExtra(Intent.EXTRA_TEXT,"isi pesan email ini dari aplikasi TONI App");
+        intent.setType("message/rfc822");
+        startActivity(intent);
+    }
+
+    public void buka_line(View view){
+        Uri uri = Uri.parse("line://ti/p/vpa");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
 }
